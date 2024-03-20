df99ceeb794412e84409bf54715348b3eef70d85
Freddy <fabernal1230@gmail.com>
2024-03-20 14:24:49 +0100

    Update paths in reaction_profile_summary.py

--- reaction_profile_summary.py
+++ reaction_profile_summary.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Short script to retrieve data from calculations in autodE.
-It generates a summary csv file.
+It generates a CSV file summarizing results. 
+All autodE generated folders muts be on the current directory.
 
 @author: Dr. Freddy A. Bernal
 """
 
 import os
-import sys
 
-FOLDER_MODULE = '/home/tga_user/Documents/Scripts/Reactivity_autodE'
-
-sys.path.append(FOLDER_MODULE)
-
-from autodE_utils import retrieve_data_rxn_profile
+from lib.utils import retrieve_reaction_profile
 
 
 if __name__ == '__main__':
     # Get results from all folders in current directory
-    summary = retrieve_data_rxn_profile()
+    summary = retrieve_reaction_profile()
     # Define output file
     dirname = os.path.split(os.getcwd())[-1]
     results_file = f"Results_{dirname}.csv"
